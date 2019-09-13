776636ee41a4de472bd5aa1b5470e17ef3a018c7
rolandoquiroz <rolandoquiroz@gmail.com>
2019-09-13 04:29:56 +0000

    3-infinite_add.py added again

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    s = 0
+    sum = 0
     for i in range(1, len(sys.argv)):
-        s = s + int(sys.argv[i])
-        print("{}".format(s))
+        sum = sum + int(sys.argv[i])
+        print("{}".format(sum))
