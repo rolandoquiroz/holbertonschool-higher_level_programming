cfb92fe445259c1e38d03c716ed1c154848620a1
rolandoquiroz <rolandoquiroz@gmail.com>
2019-09-18 01:41:06 +0000

    Commit # 19: 9-max_integer.py added

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        ans = my_list[0]
+        m = my_list[0]
         for i in my_list:
-            if i > ans:
-                ans = i
-                return (ans)
+            if i > m:
+                m = i
+                return (m)
     return (None)
