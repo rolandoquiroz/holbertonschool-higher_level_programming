f02ac9062eb5d3d87a2ba2edb5a70dc66db549fb
rolandoquiroz <rolandoquiroz@gmail.com>
2019-09-18 00:39:19 +0000

    Commit # 14: 8-multiple_returns.py readded

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (0, None)
-    else:
+    if sentence:
         return (len(sentence), sentence[0])
+    else:
+        return (len(sentence), None)
