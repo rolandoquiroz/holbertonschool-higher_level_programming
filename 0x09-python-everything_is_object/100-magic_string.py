7ebad916ee197c9803088b4a58fd9780dd73bace
rolandoquiroz <rolandoquiroz@gmail.com>
2019-10-11 07:42:43 +0000

    Commit # 12: 100 answers readded

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
-
-Learn more or give us feedback
 #!/usr/bin/python3
-def magic_string(string=[]):
-    string += ["Holberton"]
-    return(", ".join(string))
+def magic_string(static={"count": 0}):
+    static["count"] += 1
+    return ', '.join(["Holberton" for i in range(static["count"])])
