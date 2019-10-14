17bc2964fa44557bf8ef4767289a83d80708b510
rolandoquiroz <rolandoquiroz@gmail.com>
2019-10-14 04:05:04 +0000

    2-matrix_divided.py readded

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,27 +16,35 @@ def matrix_divided(matrix, div):
     Returns:
         A matrix with te result of division.
     """
-    ans = []
-    if((div is None) or (type(div) != int) and (type(div) != float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if (type(matrix) != list):
-        raise TypeError("matrix must be a matrix (list of lists)"
-                        " of integers/floats")
+        err = 'matrix must be a matrix (list of lists) of integers/floats'
+    if type(matrix) is not list:
+        raise TypeError(err)
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError(err)
 
-    for i in range(0, len(matrix)):
-        if (type(matrix[i]) != list):
-            raise TypeError("matrix must be a matrix (list of lists)"
-                            " of integers/floats")
-        put_in = []
-        for j in range(0, len(matrix[i])):
-            if ((type(matrix[i][j]) != int) and (type(matrix[i][j]) != float)):
-                raise TypeError("matrix must be a matrix (list of lists)"
-                                " of integers/floats")
-            if (len(matrix[0]) != len(matrix[i])):
-                raise TypeError("Each row of the matrix"
-                                " must have the same size")
-            put_in.append(round((matrix[i][j]) / div, 2))
-        ans.append(put_in)
-    return (ans)
+    # make sure div is not 0
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    if type(div) is not int and type(div) is not float:
+        raise TypeError('div must be a number')
+
+    # make sure each element is an int or float
+    for row in matrix:
+        for elem in row:
+            if type(elem) is not int and type(elem) is not float:
+                raise TypeError(err)
+
+    # make sure all rows are the same size
+    length_of_rows = []
+    for row in matrix:
+        length_of_rows.append(len(row))
+    length_of_rows = set(length_of_rows)
+    if len(length_of_rows) > 1:
+        raise TypeError('Each row of the matrix must have the same size')
+
+    # finally, do the division and return the new matrix
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append([round((x/div), 2) for x in row])
+    return new_matrix
