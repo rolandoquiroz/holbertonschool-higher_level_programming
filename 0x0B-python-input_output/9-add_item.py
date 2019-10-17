d644607b04d4794ae793f9653a5ab95c460e323d
rolandoquiroz <rolandoquiroz@gmail.com>
2019-10-17 05:28:23 +0000

    Commit # 18: 9 file added

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,24 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This script that adds all arguments to
+a Python list, and then save them to a file.
+
+"""
+
+
+import json
+from sys import argv
+
+
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
+
+filename = 'add_item.json'
+
+try:
+    new = load_json(filename)
+except (ValueError, FileNotFoundError):
+    new = []
+
+new = new + argv[1:]
+save_json(new, filename)
