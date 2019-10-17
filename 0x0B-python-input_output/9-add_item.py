23af9f16f4e9cf3a076879d4de8c7230439f2d5a
rolandoquiroz <rolandoquiroz@gmail.com>
2019-10-17 05:35:06 +0000

    Commit # 19: 9 file readded

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,15 +10,15 @@ import json
 from sys import argv
 
 
-save_json = __import__('7-save_to_json_file').save_to_json_file
-load_json = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 filename = 'add_item.json'
 
 try:
-    new = load_json(filename)
+    new_list = load_from_json_file(filename)
 except (ValueError, FileNotFoundError):
-    new = []
+    new_list = []
 
-new = new + argv[1:]
-save_json(new, filename)
+new_list = new_list + argv[1:]
+save_to_json_file(new_list, filename)
