017389e2d0341d39fab9af5078e33c27f74ab8d6
rolandoquiroz <rolandoquiroz@gmail.com>
2019-10-17 00:45:16 +0000

    Commit # 04: 1-number_of_lines.py file added

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,1 +1,26 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This module contains a function that returns
+the number of lines of a text file.
+
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    number_of_lines- Counts the number of lines of a text file.
+
+    Args:
+        filename(str): filename
+
+    Returns:
+        The number of lines of a text file.
+
+    """
+    number_of_lines = 0
+    nl = 0
+    with open(filename, mode="r", encoding="utf-8") as a_file:
+        nl = len(list(a_file))
+        for a_line in a_file:
+            number_of_lines += 1
+    return (nl)
