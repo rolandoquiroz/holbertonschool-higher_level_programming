20bf81e6a71961c78ed3a25f899a7ffb1c0c9a3f
rolandoquiroz <rolandoquiroz@gmail.com>
2019-10-17 04:06:20 +0000

    Commit # 08: 2-read_lines.py file re-added again

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -21,7 +21,7 @@ def read_lines(filename="", nb_lines=0):
 
     """
     with open(filename, 'r', encoding='utf-8') as f:
-        if ((nb_lines <= 0) or (nb_lines > len(list(f)))):
+        if ((nb_lines <= 0) or (nb_lines > len(list(filename)))):
             print(f.read(), end="")
         else:
             for i in range(nb_lines):
