bf6f8d27bfe354ecd2253cebba4306837ef5cd43
rolandoquiroz <rolandoquiroz@gmail.com>
2019-11-26 21:36:32 +0000

    Commit # 17: [Re-Add] 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Script that creates the table unique_id on your MySQL server.
-SELECT id name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT id name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id;
