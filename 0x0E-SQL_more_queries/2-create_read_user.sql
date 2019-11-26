3d50fa4a5d73438e89f5791e4de96975a6756a16
rolandoquiroz <rolandoquiroz@gmail.com>
2019-11-26 19:23:39 +0000

    Commit # 04: [Re-Add] 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- Script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON * . * TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
