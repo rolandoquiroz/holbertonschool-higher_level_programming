1f6b887c59c356c66d958430f341630bfcda26ec
rolandoquiroz <rolandoquiroz@gmail.com>
2019-11-27 03:04:30 +0000

    Commit # 24: [Re-Add] 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+WHERE tv_show_genres.genre_id IS NOT NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
