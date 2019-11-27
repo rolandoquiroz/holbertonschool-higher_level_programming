d3b8835744d22f446240614e9d8ed0b6bb7e85e0
rolandoquiroz <rolandoquiroz@gmail.com>
2019-11-27 04:53:20 +0000

    Commit # 38: [Add] 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_show_genres
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name AS `genre`, COUNT(*) AS `number_shows` FROM tv_show_genres
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name ORDER BY number_shows DESC;
