e794cb3c4bf938cc52f63025d2c8a72c314b419b
rolandoquiroz <rolandoquiroz@gmail.com>
2019-12-15 23:44:19 +0000

    [Upload] 9-model_state_filter_a.py script file

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects that contain
+the letter a from the database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+    """ This won't be run when imported """
+
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state_obj_a_f = session.query(State).all().filter(State.name.ilike('%a%'))
+
+    for state_obj_a in state_obj_a_f:
+        print("{}: {}".format(state_obj_a.id, state_obj_a.name))
+
+    session.close()
