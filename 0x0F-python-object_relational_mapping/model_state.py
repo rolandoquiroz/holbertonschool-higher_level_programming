9c345502a1da66bca2d0ba873917e94157142f40
rolandoquiroz <rolandoquiroz@gmail.com>
2019-12-15 19:48:07 +0000

    [Upload] model_state.py script file add documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
-"""python file containing the class definition of State and declarative base"""
+"""
+File that contains the class definition of a State
+and an instance Base = declarative_base()
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
 
 class State(Base):
-    """State class inherits from Base"""
+    """
+    class State inherits from Base
+    """
+    """
+    links to the MySQL table states
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, , autoincrement=T\
-     rue)
+    """
+    class attribute id that represents a column of an auto-generated,
+    unique integer, can’t be null and is a primary key
+    """
+    id = Column(Integer,
+                autoincrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    """
+    class attribute name that represents a column of a string
+    with maximum 128 characters and can’t be null
+    """
     name = Column(String(128), nullable=False)
