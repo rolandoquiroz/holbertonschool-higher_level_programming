94f6cb4dbcefc111073fca41a02028dd9d83698e
rolandoquiroz <rolandoquiroz@gmail.com>
2019-12-15 01:47:46 +0000

    [Fix] 3-my_safe_filter_states.py script file again documentation

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+table of hbtn_0e_0_usa where name matches the argument. But this time,
+this is safe from MySQL injections!
 """
 
 if __name__ == "__main__":
