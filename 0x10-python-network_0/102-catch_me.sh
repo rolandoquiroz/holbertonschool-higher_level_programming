ba4dbcb019452ebb1bd778c859f3879bd7b43a8f
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-07 17:13:06 +0000

    Upload Task 9 script file same Task 7 adding PUT

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -o /dev/null -w "You got me!" 0:5000/catch_me
+curl -s -o /dev/null -w "You got me!" -X PUT 0:5000/catch_me
