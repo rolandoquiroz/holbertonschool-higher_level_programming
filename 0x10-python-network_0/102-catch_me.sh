59f7783f2896ef948fddd237fede3fbe594596ae
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-07 17:37:44 +0000

    Upload Task 9 script

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -L -d "user_id=98" -H "Origin: HolbertonSchool" -X PUT 0:5000/catch_me
+curl -s -o /dev/null -w "You got me!" -X PUT 0:5000/catch_me
