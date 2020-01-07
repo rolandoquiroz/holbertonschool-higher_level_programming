ef20f57cb9ec0879dd4a93b06340b04bdac7e73e
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-07 08:49:39 +0000

    Commit # 07: [Upload] Task 5 right output!

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# Takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -d "email=hr%40holbertonschool%2Ecom&subject=I%20will%20always%20be%20here%20for%20PLD" -sL -X POST "$1"
