33319442a24d783acae1e5ff0f4611bfc2232078
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-13 21:23:15 +0000

    Upload Task 2 First right output

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """
-Describe me
+Write a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
 """
 
 if __name__ == "__main__":
-	""" this won't be run when imported """
+        """ this won't be run when imported """
+        from sys import argv
+        from urllib import request, parse
+        values = {'email' : argv[2]}
+        data = parse.urlencode(values)
+        data = data.encode('utf-8')
+        req = request.Request(argv[1], data)
+        with request.urlopen(req) as response:
+                response_data = response.read()
+                print(response_data.decode('utf-8'))
