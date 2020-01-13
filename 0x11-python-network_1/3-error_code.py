4bd21e682cf4308c1a25068e084f4a5c654b61e7
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-13 22:14:29 +0000

    Upload Task 3 First right output

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """
-Describe me
+Write a Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
 """
 
 if __name__ == "__main__":
-	""" this won't be run when imported """
+        """ this won't be run when imported """
+        from sys import argv
+        from urllib.request import Request, urlopen
+        from urllib.error import HTTPError
+        req = Request(argv[1])
+        try:
+                with urlopen(req) as response:
+                        data = response.read()
+                        print(data.decode('utf-8'))
+        except HTTPError as e:
+                print('Error code: ', e.code)
