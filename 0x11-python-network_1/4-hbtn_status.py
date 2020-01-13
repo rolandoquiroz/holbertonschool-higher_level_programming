3fdfd9efa072ff6d6441b40966f28f7d8f3e7006
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-13 23:16:05 +0000

    [Feature] Task 4 type applied to de content

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         import requests
         response = requests.get('https://intranet.hbtn.io/status')
         print("Body response:")
-        print("\t- type: {}".format(type(response.headers['content-type'])))
+        print("\t- type: {}".format(type(response.text)))
         print("\t- content: {}".format(response.text))
