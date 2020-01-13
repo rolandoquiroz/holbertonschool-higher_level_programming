1213fd4d189c7aa5d025ef9e41216dcdc1c8fea3
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-13 23:36:05 +0000

    [Upload] Task 5 right output

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """
-Describe me
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found in
+the header of the response.
 """
 
 if __name__ == "__main__":
-	""" this won't be run when imported """
+        """ this won't be run when imported """
+        from sys import argv
+        import requests
+        response = requests.get(argv[1])
+        print(response.headers.get('X-Request-Id'))
