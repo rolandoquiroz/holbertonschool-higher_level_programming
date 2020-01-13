f1b74af1e094d174235be6ee00b1b489d5828091
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-13 18:22:57 +0000

    Upload Task 0 First right output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,4 +4,11 @@ Describe me
 """
 
 if __name__ == "__main__":
-	""" this won't be run when imported """
+        """ this won't be run when imported """
+        from urllib import request
+        with request.urlopen("https://intranet.hbtn.io/status") as response:
+                data = response.read()
+                print("Body response:")
+                print("\t- type: {}".format(type(data)))
+                print("\t- content: {}".format(data))
+                print("\t- utf8 content: {}".format(data.decode('utf8')))
