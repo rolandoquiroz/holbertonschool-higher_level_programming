709da505fb685ad6227dfb6ef030d096f670dbe2
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-14 00:07:03 +0000

    Upload Task 6 right output

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """
-Describe me
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
 """
 
 if __name__ == "__main__":
-	""" this won't be run when imported """
+        """ this won't be run when imported """
+        from sys import argv
+        from requests import post
+        req = post(argv[1], data={'email': argv[2]})
+        print(req.text)
