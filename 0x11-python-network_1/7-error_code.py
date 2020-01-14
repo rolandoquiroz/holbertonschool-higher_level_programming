38fd516530d9ab8146b9a360eb1892a760f3ac83
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-14 00:27:30 +0000

    Upload Task 7 right output

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """
-Describe me
+Write a Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
 """
 
 if __name__ == "__main__":
-	""" this won't be run when imported """
+        """ this won't be run when imported """
+        from sys import argv
+        from requests import get
+        req = get(argv[1])
+        if (req.status_code < 400):
+                print(req.text)
+        else:
+                print('Error code:', req.status_code)
