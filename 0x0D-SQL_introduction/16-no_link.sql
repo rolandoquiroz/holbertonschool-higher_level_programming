9e15eef39bf8ef7db8237a371b4719bd078bc3cb
rolandoquiroz <rolandoquiroz@gmail.com>
2020-01-26 05:20:21 +0000

    [Re-do] 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 -- Lists all records of the table second_table of the database
+-- SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
 SELECT score, name FROM second_table WHERE name = name ORDER BY score DESC;
