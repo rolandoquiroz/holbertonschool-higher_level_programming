449d73f8c99a9b02d7bab125c2535454ea44b1e8
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-10 17:11:57 -0500

    [Feature] Task 6: 6-square.js file added

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-module.exports = class Square extends require("./5-square") {
-  charPrint(c) {
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
       let i;
       for (i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
