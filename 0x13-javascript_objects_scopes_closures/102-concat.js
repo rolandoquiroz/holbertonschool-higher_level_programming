875b880cb3cb15e9e553bc632c96b2b42837a726
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-10 22:36:44 -0500

    [Feature] Task 13: 102-concat.js file added

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], function (err1, data1) {
+  fs.readFile(process.argv[3], function (err2, data2) {
+    if (err1 || err2) {
+      return;
+    }
+    const data = data1 + '\n' + data2 + '\n';
+    fs.writeFile(process.argv[4], data, function (err) {
+      if (err) {
+      }
+    });
+  });
+});
