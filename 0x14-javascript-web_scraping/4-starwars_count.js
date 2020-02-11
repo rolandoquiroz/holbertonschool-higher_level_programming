525f7bcf20b869513d9e79a4dc9502ede7ca5121
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-11 17:35:23 -0500

    [Feature] Task 4: 4-starwars_count.js file added

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], function (err, response, body) {
+  if (!err && response.statusCode === 200) {
+    const movies = JSON.parse(body).results;
+    let appearances = 0;
+    for (let i = 0; i < movies.length; i++) {
+      const characters = movies[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].includes('/18/')) {
+          appearances += 1;
+        }
+      }
+    }
+    console.log(appearances);
+  }
+});
