24c7ba342dca3905b9b4ab0d1564dab2cac14b7b
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-11 12:08:27 -0500

    [Feature] Tasks 0 and 1 files added

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], { encoding: 'utf-8' }, (err, content) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
+});
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], { encoding: 'utf8' }, function (err) {
+  if (err) throw err;
+});
