38ef525f5531981e6d67d3b6ff1b26d84d7b8663
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-11 15:11:21 -0500

    [Feature] Task 3: 3-starwars_title.js file added

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const url = 'http://swapi.co/api/films/';
+require('request').get(url + process.argv[2] + '/', function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
