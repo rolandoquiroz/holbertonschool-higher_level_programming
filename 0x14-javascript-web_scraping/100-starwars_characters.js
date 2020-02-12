116e22d72e5ec2cc78e6eb3dffb4e90778eff44e
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-11 23:52:33 -0500

    [Feature] Task 6: 6-completed_tasks.js file added

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  if (!err && response.statusCode === 200) {
+    const allcharacters = JSON.parse(body)['characters'];
+    let i = 0;
+    while (i < allcharacters.length) {
+      request(allcharacters[i], function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+          console.log(JSON.parse(body)['name']);
+        }
+      });
+      i++;
+    }
+  }
+});
