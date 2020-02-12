bf80d12db0431f76fbb797790c12c2021cc9026b
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-11 23:54:07 -0500

    [Feature] Task 7: 100-starwars_characters.js file added

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,12 +3,12 @@ const request = require('request');
 const url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (err, response, body) {
   if (!err && response.statusCode === 200) {
-    const allcharacters = JSON.parse(body)['characters'];
+    const allcharacters = JSON.parse(body).characters;
     let i = 0;
     while (i < allcharacters.length) {
       request(allcharacters[i], function (err, response, body) {
         if (!err && response.statusCode === 200) {
-          console.log(JSON.parse(body)['name']);
+          console.log(JSON.parse(body).name);
         }
       });
       i++;
