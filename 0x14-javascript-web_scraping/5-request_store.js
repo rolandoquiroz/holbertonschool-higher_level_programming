ca9c6f28708a335792aa8edf88ebd8fe0b0216d9
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-11 20:50:49 -0500

    [Feature] Task 5: 5-request_store.js file added

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-
 request.get(process.argv[2], function (err, response, body) {
   if (!err && response.statusCode === 200) {
     fs.writeFile(process.argv[3], body, { encoding: 'utf-8' }, function (err) {
