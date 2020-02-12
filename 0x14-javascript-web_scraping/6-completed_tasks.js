7bc25a6bb62330b28488f47dd63375795450ae33
rolandoquiroz <rolandoquiroz@gmail.com>
2020-02-11 22:51:21 -0500

    [Feature] Task 6{: 5-request_store.js file added

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], function (err, response, body) {
+  if (!err && response.statusCode === 200) {
+    const alltasks = JSON.parse(body);
+    const tasksdone = {};
+    let i = 0;
+    while (i < alltasks.length) {
+      if (alltasks[i].completed === true) {
+        if (tasksdone[alltasks[i].userId]) {
+          tasksdone[alltasks[i].userId] += 1;
+        } else {
+          tasksdone[alltasks[i].userId] = 1;
+        }
+      }
+      i++;
+    }
+    console.log(tasksdone);
+  }
+});
