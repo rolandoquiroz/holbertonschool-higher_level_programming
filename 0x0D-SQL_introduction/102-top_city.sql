d3fd43f7f9fb2409380a9eba0f92a0bfef4f1c71
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-02 21:55:33 -0500

    Redo Advanced tasks

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,8 @@
--- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
--- WHERE month > 6 AND month < 9 , WHERE month BETWEEN 7 AND 8, WHERE month IN (7, 8)
-SELECT city, AVG(value) AS avg_temp
+-- Import in hbtn_0c_0 database the table temperatures.sql
+-- Script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+USE hbtn_0c_0;
+SELECT city, AVG(`value`) AS avg_temp
 FROM temperatures
-WHERE (month = 7 OR month = 8)
+WHERE `month` = 7 OR `month` = 8 -- WHERE `month` >= 7 AND `month` <= 8, WHERE `month` IN (7, 8), WHERE `month` BETWEEN 7 AND 8
 GROUP BY city
 ORDER BY avg_temp DESC LIMIT 3;
