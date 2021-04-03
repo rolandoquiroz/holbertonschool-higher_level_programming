badcb5bd5073cd31a937dea32485e99bb7d7a599
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-02 21:59:51 -0500

    Redo Advanced tasks

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,6 +3,6 @@
 USE hbtn_0c_0;
 SELECT city, AVG(`value`) AS avg_temp
 FROM temperatures
-WHERE `month` = 7 OR `month` = 8 -- WHERE `month` >= 7 AND `month` <= 8, WHERE `month` IN (7, 8), WHERE `month` BETWEEN 7 AND 8
+WHERE `month` >= 7 AND `month` <= 8 -- WHERE `month` = 7 OR `month` = 8, WHERE `month` IN (7, 8), WHERE `month` BETWEEN 7 AND 8
 GROUP BY city
 ORDER BY avg_temp DESC LIMIT 3;
