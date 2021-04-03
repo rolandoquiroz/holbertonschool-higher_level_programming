70df7a31a43b2a4950e050fbaa946832d2fc0e3f
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-02 22:10:44 -0500

    Redo Advanced tasks

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
+-- Import in hbtn_0c_0 database the table temperatures.sql
 -- Script that displays the max temperature of each state (ordered by State name).
--- WHERE month > 6 AND month < 9 , WHERE month BETWEEN 7 AND 8, WHERE month IN (7, 8)
-SELECT `state`, MAX(value) AS max_temp
+SELECT `state`, MAX(`value`) AS max_temp
 FROM temperatures
 GROUP BY `state`
-ORDER BY `state` LIMIT 3;
+ORDER BY `state` ASC LIMIT 3;
