41d31cf562ab8eb1d0945e9c16a59474e547511a
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-02 22:21:21 -0500

    Redo Advanced tasks

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,8 +8,8 @@ USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 CHANGE `name` `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,6 @@
 -- Import in hbtn_0c_0 database the table temperatures.sql
 -- Script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-USE hbtn_0c_0;
 SELECT city, AVG(`value`) AS avg_temp
-FROM temperatures
+FROM hbtn_0c_0.temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
