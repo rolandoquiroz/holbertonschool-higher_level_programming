1103eea2bf8c3be88c840156a136a50a197521b3
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-05 16:03:08 -0500

    Redo tasks

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,12 @@
--- Script that creates the table unique_id on your MySQL server.
+-- Script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- states description:
+--  id INT unique, auto generated, can not be null and is a primary key
+--  name VARCHAR(256) can’t be null
+-- If the database hbtn_0d_usa already exists, this script does not fail
+-- If the table states already exists, this script does not fail
+-- SERIAL DEFAULT VALUE = NOT NULL AUTO_INCREMENT UNIQUE
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    ID INT SERIAL DEFAULT VALUE PRIMARY KEY,
+    `name` VARCHAR(256) NOT NULL,
+);
