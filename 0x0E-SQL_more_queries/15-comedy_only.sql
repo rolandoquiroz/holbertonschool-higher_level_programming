857cec6fa18eee948ee8946cd7db57634abb29df
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-10 01:59:27 -0500

    Redo all tasks

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,15 @@
--- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title FROM tv_shows
-INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title;
+-- Script that lists all Comedy shows in the database hbtn_0d_tvshows.
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+--  The tv_genres table contains only one record where name = Comedy (but the id can be different)
+--  Each record should display: tv_shows.title
+--  Results must be sorted in ascending order by the show title
+--  You can use only one SELECT statement
+--  The database name will be passed as an argument of the mysql command
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.`name` = 'Comedy'
+ORDER BY tv_shows.title ASC;
