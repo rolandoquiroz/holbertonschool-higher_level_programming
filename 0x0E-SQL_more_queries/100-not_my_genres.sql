84ee5a9490b49fd3ef9db08c8c4f7a5b40aeee50
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-10 02:47:14 -0500

    Redo all tasks

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,7 @@
+-- Script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+--  The tv_shows table contains only one record where title = Dexter (but the id can be different)
+--  Each record should display: tv_genres.name
+--  Results must be sorted in ascending order by the genre name
+--  You can use a maximum of two SELECT statement
+--  The database name will be passed as an argument of the mysql command
---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,7 @@
+-- Script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use a maximum of two SELECT statement
+-- The database name will be passed as an argument of the mysql command
