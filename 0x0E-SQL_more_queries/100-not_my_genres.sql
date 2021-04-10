2a42d00a47f3dd5a349da004cbdf6e1d341bacc8
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-10 03:41:26 -0500

    Redo all tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -5,3 +5,14 @@
 --  Results must be sorted in ascending order by the genre name
 --  You can use a maximum of two SELECT statement
 --  The database name will be passed as an argument of the mysql command
+SELECT tv_genres.`name`
+FROM tv_genres
+WHERE tv_genres.`name` NOT IN
+(SELECT tv_genres.`name`
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.`name` ASC;
