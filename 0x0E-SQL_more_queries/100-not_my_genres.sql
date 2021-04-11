25e1164f8b9ff7d2e181e71c23c61915c398f114
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-10 19:31:43 -0500

    Redo all tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,8 +7,7 @@
 --  The database name will be passed as an argument of the mysql command
 SELECT tv_genres.`name`
 FROM tv_genres
-WHERE tv_genres.id
-NOT IN
+WHERE tv_genres.id NOT IN -- MINUS
 (SELECT tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
