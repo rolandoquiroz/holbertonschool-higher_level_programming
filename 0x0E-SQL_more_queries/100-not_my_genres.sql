00a68be35b1f7effc49ab84f10e1070142c5cd6b
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-10 19:42:02 -0500

    Redo all tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,8 +9,8 @@ SELECT tv_genres.`name`
 FROM tv_genres
 WHERE tv_genres.id NOT IN -- MINUS
 (SELECT tv_show_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+FROM tv_show_genres
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter')
 ORDER BY tv_genres.`name` ASC;
