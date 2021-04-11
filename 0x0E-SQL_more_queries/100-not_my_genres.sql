54f441432a9198d249edcf827058e70bc661e8bf
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-10 19:20:34 -0500

    Redo all tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,12 +7,10 @@
 --  The database name will be passed as an argument of the mysql command
 SELECT tv_genres.`name`
 FROM tv_genres
-WHERE tv_genres.`name` NOT IN
-(SELECT tv_genres.`name`
-FROM tv_genres
-LEFT JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.id NOT IN
+(SELECT tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter')
 ORDER BY tv_genres.`name` ASC;
