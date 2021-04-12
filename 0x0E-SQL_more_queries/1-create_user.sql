e1ae6f7d8325307b453c5a495da00d145bde027c
rolandoquiroz <rolandoquiroz@gmail.com>
2021-04-12 11:54:40 -0500

    Redo all tasks

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 --  The user_0d_1 password is set to user_0d_1_pwd
 --  If the user user_0d_1 already exists, this script does not fail
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost ;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
