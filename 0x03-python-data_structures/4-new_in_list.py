d777be331e3f726ea854d7493c95ab3f2450aa1f
rolandoquiroz <rolandoquiroz@gmail.com>
2023-06-09 14:46:47 -0500

    Update tasks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,5 @@ def new_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
         my_list_copied = my_list[:]
         my_list_copied[idx] = element
-        return (my_list_copied)
+        return my_list_copied
     return my_list
