d131d91ff43cd31e48485ba6af5701288097e1f7
rolandoquiroz <rolandoquiroz@gmail.com>
2023-06-09 14:34:19 -0500

    Update tasks

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        my_list_copied = my_list[:]
+        my_list_copied[idx] = element
+        return (my_list_copied)
+    return (my_list)
