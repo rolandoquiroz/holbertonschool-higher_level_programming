717f6cc05974fd1c949fcf5609b5b134ad101e6a
rolandoquiroz <rolandoquiroz@gmail.com>
2023-06-12 12:48:15 -0500

    Add an empty class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+This is a module-level docstring.
+
+This module provides an empty class for defining a square.
+
+Classes:
+    - Square: An empty class to define a square.
+"""
+
+
+class Square:
+    """
+    An empty class to define a square.
+
+    This class does not have any attributes or methods.
+    It can be used as a placeholder or as a base class.
+    """
+    pass
