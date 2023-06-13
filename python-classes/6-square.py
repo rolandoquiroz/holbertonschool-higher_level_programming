bb259d0dc89a178fb19d1e7809db1f6f69de26d5
rolandoquiroz <rolandoquiroz@gmail.com>
2023-06-13 16:13:09 -0500

    Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -51,6 +51,17 @@ class Square:
         If size or position are not provided,
         the default values are 0 for size and (0, 0) for position.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        if not isinstance(position, tuple) or len(position) != 2 \
+                or not isinstance(position[0], int) \
+                or not isinstance(position[1], int) \
+                or position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
         self.__size = size
         self.__position = position
 
