46a00138907b66c77fe05e6a0e6bf1dedf21349c
rolandoquiroz <rolandoquiroz@gmail.com>
2023-06-12 19:15:25 -0500

    Add a class Square that defines a square with a private instance attribute size and instantiation with size (no type/value verification)

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+This is a module-level docstring.
+
+This module provides an class for representing a square.
+
+Classes:
+    - Square: Defines a square.
+"""
+
+
+class Square:
+    """
+    Defines a square.
+
+    This class can be used to create square objects with a specific size.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Parameters
+        ----------
+        size : The size of each side of the square.
+        """
+        self.__size = size
