0781fa653f11deb8a21e6bda360ce902cdb05909
rolandoquiroz <rolandoquiroz@gmail.com>
2024-11-14 01:44:23 +0000

    Fix table name: remove hbtn_0e_0_usa. prefix from tablename states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Python script that list all states from table hbtn_0e_0_usa.states """
+""" Python script that list all states from table states
+from database hbtn_0e_0_usa """
 
 if __name__ == "__main__":
     """ This code won't be run when imported """
