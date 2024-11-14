74a8ddab22f6a1b9a703d7fef8eca08ce940b778
rolandoquiroz <rolandoquiroz@gmail.com>
2024-11-14 02:25:58 +0000

    Fix table name: remove hbtn_0e_0_usa. prefix from tablename states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,15 +10,15 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-    database = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=argv[1],
         password=argv[2],
-        db=argv[3]
+        database=argv[3]
     )
 
-    cursor = database.cursor()
+    cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states ORDER BY id;")
 
@@ -28,4 +28,4 @@ if __name__ == "__main__":
         print(_)
 
     cursor.close()
-    database.close()
+    db.close()
