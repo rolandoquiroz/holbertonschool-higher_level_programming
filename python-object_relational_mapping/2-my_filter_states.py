503c8e18540692e21b888d7d4ec16636dceca556
rolandoquiroz <rolandoquiroz@gmail.com>
2024-11-14 04:06:05 +0000

    Add semicolon to SQL code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states \
-                 WHERE name = '{name:s}' \
+                 WHERE name = BINARY '{name:s}' \
                  ORDER BY id ASC;".format(name=argv[4]))
 
     states = cur.fetchall()
