325bbb372ab44a2c6711ae28806b26ecb69886d9
rolandoquiroz <rolandoquiroz@gmail.com>
2024-11-14 01:28:56 +0000

    Add task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Python script that list all states from table hbtn_0e_0_usa.states """
+
+if __name__ == "__main__":
+    """This code won't be run when imported"""
+
+    from sys import argv
+    import MySQLdb
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = database.cursor()
+
+    cursor.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY id;")
+
+    states = cursor.fetchall()
+
+    for _ in states:
+        print(_)
+
+    cursor.close()
+    database.close()
