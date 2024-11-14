9efcdb084e7282212094c95030e29ee5adb06288
rolandoquiroz <rolandoquiroz@gmail.com>
2024-11-14 05:09:30 +0000

    Add task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,7 +2,7 @@
 """
 Python script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa database where name matches the argument.
-But this time, it is safe from MySQL injections!
+But this time, write one that is safe from MySQL injections!
 """
 
 if __name__ == "__main__":
