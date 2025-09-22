458c7eb74a05a9ee2a28df7d6f1e05e87be0328a
rolandoquiroz <rolandoquiroz@gmail.com>
2025-09-22 15:33:44 -0500

    Add complete function add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -30,4 +30,10 @@ def add_integer(a, b=98):
         OverflowError: If a or b is infinity.
         ValueError: If a or b is NaN.
     """
-    return a + b 
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
