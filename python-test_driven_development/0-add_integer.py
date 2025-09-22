e2a0b2abd2f71781cf0ab20d656c0c493de351b7
rolandoquiroz <rolandoquiroz@gmail.com>
2025-09-22 10:43:18 -0500

    fix: validate input types for add_integer parameters

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -20,4 +20,10 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If either `a` or `b` is not an integer or float.
     """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
