e0a5c097aca28529d6190135ddc21a44721e60d9
rolandoquiroz <rolandoquiroz@gmail.com>
2025-09-22 11:56:24 -0500

    style(add_integer): fix blank line for PEP8 compliance

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,6 +7,7 @@ type safety by raising a TypeError if either of the inputs is not
 an integer or float. The result is always returned as an integer.
 """
 
+
 def add_integer(a, b=98):
     """Add two integers.
 
