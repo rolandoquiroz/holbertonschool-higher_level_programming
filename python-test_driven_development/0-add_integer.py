091ec104b1662d2662cbe9fc7df2ca79b7b4ba93
rolandoquiroz <rolandoquiroz@gmail.com>
2025-09-25 19:32:09 -0500

    feat: ensure correct output for 1 + 2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -21,4 +21,10 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If a or b are not integers or floats.
     """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
