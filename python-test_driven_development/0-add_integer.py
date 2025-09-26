c7e44989cae84f1e81540f20cef303a32e3f2837
rolandoquiroz <rolandoquiroz@gmail.com>
2025-09-25 19:07:53 -0500

    docs: add module documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,2 +1,8 @@
 
 #!/usr/bin/python3
+"""
+This module provides a function for adding two integers.
+
+It exposes a single function:
+    - add_integer(a, b=98): Adds two integers and returns the result.
+"""
